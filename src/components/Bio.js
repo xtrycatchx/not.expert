13ba3e052f7635a7b88a37089dab34271918f02e
xtrycatchx <xtrycatchx@gmail.com>
2018-12-09 14:43:13 +0800

    blog migration

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,18 +4,15 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import { rhythm } from '../utils/typography'
-
 class Bio extends React.Component {
   render() {
     return (
       <div
         style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
+          display: 'flex'
         }}
       >
-        <p style={{ maxWidth: 500 }}>
+        <p style={{ maxWidth: 510, color: '#808080' }}>
         Yo! I am <a href="https://mobile.twitter.com/xtrycatchblockx">Paul Sydney Orozco</a>, a dad, husband, employee, forever newbie and a Software Engineer.
           {' '}
         These are my (ugly) code musings, random rants and some experiments. .
